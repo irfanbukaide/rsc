49237846260a45217504e7f12ced165513d0ebc8
Muhammad Irfan <irfan@bukaide.com>
2018-11-15 05:52:17 +0700

    Initial commit

---
+++ application/core/MY_Model.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: irfan
+ * Date: 15/11/18
+ * Time: 5:49
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        //Do your magic here
+    }
+}
+
+/* End of file .php */
